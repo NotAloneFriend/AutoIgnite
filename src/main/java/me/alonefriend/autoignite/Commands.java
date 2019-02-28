d83b5d9a69d11e42d1a0aaffc9bc8965e5bf818e
NotAloneFriend <alonefriend@pm.me>
2019-02-28 22:36:19 +0100

    Remove unnecessary code

--- src/main/java/me/alonefriend/autoignite/Commands.java
+++ src/main/java/me/alonefriend/autoignite/Commands.java
@@ -14,27 +14,27 @@ public class Commands implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (label.equalsIgnoreCase("autoignite")) {
-            if (args.length == 0 || args[0].equalsIgnoreCase("help")) {
-                if (sender.hasPermission("autoignite.help") || sender.hasPermission("autoignite.*") || sender.isOp()) {
-                    sender.sendMessage(ChatColor.YELLOW + "--------------- Commands ---------------");
-                    sender.sendMessage(ChatColor.YELLOW + "/autoignite help: " + ChatColor.WHITE + "Display help for " + plugin.getDescription().getName() + " commands.");
-                    sender.sendMessage(ChatColor.YELLOW + "/autoignite reload: " + ChatColor.WHITE + "Reload plugin configuration.");
-                } else {
-                    sender.sendMessage(ChatColor.RED + "You don't have permission to perform this command.");
-                }
+        if (args.length == 0 || args[0].equalsIgnoreCase("help")) {
+            if (!(sender.hasPermission("autoignite.help") || sender.hasPermission("autoignite.*") || sender.isOp())) {
+                sender.sendMessage(ChatColor.RED + "You don't have permission to perform this command.");
                 return true;
             }
 
-            if (args[0].equalsIgnoreCase("reload")) {
-                if (sender.hasPermission("autoignite.reload") || sender.hasPermission("autoignite.*") || sender.isOp()) {
-                    plugin.reloadConfig();
-                    sender.sendMessage(ChatColor.GREEN + plugin.getDescription().getName() + "'s configuration has been reloaded.");
-                } else {
-                    sender.sendMessage(ChatColor.RED + "You don't have permission to perform this command.");
-                }
+            sender.sendMessage(ChatColor.YELLOW + "--------------- Commands ---------------");
+            sender.sendMessage(ChatColor.YELLOW + "/autoignite help: " + ChatColor.WHITE + "Display help for " + plugin.getDescription().getName() + " commands.");
+            sender.sendMessage(ChatColor.YELLOW + "/autoignite reload: " + ChatColor.WHITE + "Reload plugin configuration.");
+            return true;
+        }
+
+        if (args[0].equalsIgnoreCase("reload")) {
+            if (!(sender.hasPermission("autoignite.reload") || sender.hasPermission("autoignite.*") || sender.isOp())) {
+                sender.sendMessage(ChatColor.RED + "You don't have permission to perform this command.");
                 return true;
             }
+
+            plugin.reloadConfig();
+            sender.sendMessage(ChatColor.GREEN + plugin.getDescription().getName() + "'s configuration has been reloaded.");
+            return true;
         }
         return false;
     }
--- src/main/java/me/alonefriend/autoignite/Main.java
+++ src/main/java/me/alonefriend/autoignite/Main.java
@@ -9,17 +9,10 @@ public final class Main extends JavaPlugin {
 
         this.getCommand("autoignite").setExecutor(new Commands(this));
         this.getServer().getPluginManager().registerEvents(new Events(this), this);
-
-        getLogger().info(getDescription().getFullName() + " has been enabled.");
-    }
-
-    @Override
-    public void onDisable() {
-        getLogger().info(getDescription().getFullName() + " has been disabled.");
     }
 
     public void loadConfig() {
         getConfig().options().copyDefaults(true);
-        saveConfig();
+        saveDefaultConfig();
     }
 }
